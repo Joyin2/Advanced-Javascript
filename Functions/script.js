426718b094fba39b754513e02969f1a9c2c52815
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-19 12:28:53 +0530

    window

--- Functions/script.js
+++ Functions/script.js
@@ -1,6 +1,46 @@
-function greet(name, thank){
-    console.log(`hello ${name} how are you\n${thank}`)
+// function greet(name, thank){
+//     console.log(`hello ${name} how are you\n${thank}`)
+// }
+// let name = "rohan"
+// let thank = "Thanks & Regards"
+// greet(name, thank)
+
+
+
+
+// function greet(name, thank, myName){
+//     let msg = `Good Morning ${name}\nHow are you?\nHave you complete your task?\n\n${thank}\n${myName}`;
+//     return msg;
+// }
+// let name = "Rohan"
+// let thank = "Thanks & Regards"
+// let myName = "Joyin Mahmmad Aslam Laskar"
+
+// console.log(greet(name, thank, myName))
+
+
+
+
+
+// let mygreet=function (name, thank, myName){
+//     let msg = `Good Morning ${name}\nHow are you?\nHave you complete your task?\n\n${thank}\n${myName}`;
+//     return msg;
+// }
+// let name = "Rohan"
+// let thank = "Thanks & Regards"
+// let myName = "Joyin Mahmmad Aslam Laskar"
+
+// console.log(mygreet(name, thank, myName))
+
+
+
+
+const myobj ={
+    name : "joyin",
+    game : function(){
+        return `GTA ${name}` ;
+    }
+    
 }
-let name = "rohan"
-let thank = "Thanks & Regards"
-greet(name, thank)
+let name = "five"
+console.log(myobj.game(name))
---
+++ WindowObject/script.js
@@ -0,0 +1,14 @@
+
+// let a = prompt("hello bro type your name")
+// let a = confirm("are you a human?");
+
+let a = document
+// a = innerHeight
+// a = innerWidth
+// a = outerHeight
+// a = outerWidth
+// a= scrollX
+a= location
+// a= location.reload()
+// a= location.toString()
+console.log(a);
