43eb5185e2036e4c881faf26db468d507f100997
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-21 13:14:59 +0530

    dom

--- WindowObject/script.js
+++ WindowObject/script.js
@@ -2,13 +2,19 @@
 // let a = prompt("hello bro type your name")
 // let a = confirm("are you a human?");
 
-let a = document
+// let a = "document"
 // a = innerHeight
 // a = innerWidth
 // a = outerHeight
 // a = outerWidth
 // a= scrollX
-a= location
+// a= location
 // a= location.reload()
 // a= location.toString()
-console.log(a);
+// console.log(a);
+
+let fruits = ["Apple", "Mango", "Orange"]
+console.log(fruits)
+fruits.pop()
+fruits.shift()
+console.log(fruits)
