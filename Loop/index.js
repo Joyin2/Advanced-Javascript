d5be8085a2b69f588acb42ba5aa79127f1dc6bce
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-22 22:56:58 +0530

    loop

---
+++ Loop/index.js
@@ -0,0 +1,10 @@
+//for in loop
+let collection = {
+    firstName : "joyin",
+    lastName : "laskar",
+    age : 22,
+    email : "joyinlaskar@gmail.com"
+}
+for (var key in collection){
+    console.log(key[collection])
+}
