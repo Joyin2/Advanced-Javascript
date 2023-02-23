9ace619b23079e0e2003dd64f3414787f2e816cf
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-23 10:57:37 +0530

    for

--- Loop/index.js
+++ Loop/index.js
@@ -1,10 +1,22 @@
 //for in loop
-let collection = {
-    firstName : "joyin",
-    lastName : "laskar",
-    age : 22,
-    email : "joyinlaskar@gmail.com"
-}
-for (var key in collection){
-    console.log(key[collection])
+// let collection = {
+//     firstName : "joyin",
+//     lastName : "laskar",
+//     age : 22,
+//     email : "joyinlaskar@gmail.com"
+// }
+// for (var key in collection){
+//     console.log(key[collection])
+// }
+
+// while Loop
+// let num = 0;
+// while(num<=10){
+//     console.log("num")
+//     num++
+// }
+
+for(i=1; i<=10; i++){
+    console.log(i);
+    
 }
