24ea67509c68efbee1702123a8af14fd73e424b1
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-26 22:08:52 +0530

    string

---
+++ DOM/Events/script.js
@@ -0,0 +1,8 @@
+const myFunc = () =>{
+  document.body.style.backgroundColor = "red"
+  document.body.style.color = "white"
+}
+const ans = () =>{
+  let ans = document.getElementById("ans")
+  ans.style.display = "block"
+}
