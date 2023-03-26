68476191a22f5d5e56ce7c161070479f0de7c6a8
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-26 23:22:15 +0530

    p

--- Promises/script.js
+++ Promises/script.js
@@ -2,7 +2,7 @@ const probj1 = new Promise((reslove, reject) => {
   setTimeout(() => {
     let roll_no = [1, 2, 3, 4, 5];
     reslove(roll_no);
-    reject("error while communicating");
+    // reject("error while communicating");
   }, 2000);
 });
 
