42da30caf3850591bf58bffbbc71005d47d01f9f
Joyin2 <joyinlaskar2000@gmail.com>
2023-04-19 20:53:44 +0530

    on string

---
+++ ProblemSolving/02/3first3last.js
@@ -0,0 +1,28 @@
+// const numbers = [
+//   "one",
+//   "two",
+//   "three",
+//   "four",
+//   "five",
+//   "six",
+//   "seven",
+//   "eight",
+//   "nine",
+//   "ten",
+// ];
+// console.log(numbers.slice(2, 4));
+// console.log(numbers.slice(2,-3));
+// console.log(numbers);
+
+const newString = (str)=>{
+    if (str.length<3) {
+        return(
+            str
+        )
+    }else{
+        return(
+            str.slice(0,3) + str.slice(-3)
+        )
+    }
+}
+console.log(newString("hello"));
